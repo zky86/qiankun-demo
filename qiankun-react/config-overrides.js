0eeec4d1843edb671977f19efde5c692475a1eed
zhengkangyuan <272495222@qq.com>
2022-03-30 11:49:06 +0800

    修改端口配置

--- qiankun-react/config-overrides.js
+++ qiankun-react/config-overrides.js
@@ -2,16 +2,19 @@ module.exports = {
   webpack: config => {
     config.output.library = "reactApp";
     config.output.libraryTarget = "umd";
-    config.output.publicPath = "http://localhost:3000/"; // 此应用自己的端口号
+    config.output.publicPath = "http://localhost:8200/"; // 此应用自己的端口号
     return config;
   },
   devServer: configFunction => {
     return function(proxy, allowedHost) {
       const config = configFunction(proxy, allowedHost);
-      config.port = "3000";
+      config.port = "8200";
       config.headers = {
         "Access-Control-Allow-Origin": "*"
       };
+      config.historyApiFallback = true;
+      config.hot = true;
+      config.open = false;
       return config;
     };
   }
--- qiankun-vue/src/main.js
+++ qiankun-vue/src/main.js
@@ -23,6 +23,7 @@ if (!window.__POWERED_BY_QIANKUN__) { // 默认独立运行
 // 父应用加载子应用，子应用必须暴露三个接口：bootstrap、mount、unmount
 // 子组件的协议就ok了
 export async function bootstrap(props) {
+  console.log(123);
   console.log(props);
 };
 
